5865a6d3d31bef934357c540305d4d20fb979655
DanieleSavoia <danielesavoia44@gmail.com>
2023-04-05 17:56:22 +0200

    final

--- script.js
+++ script.js
@@ -8,22 +8,23 @@ const age = parseInt(prompt("Quanti anni hai?"))
 const eleSpan2 = document.getElementById("età")
 eleSpan2.innerHTML = age;
 
-const ticketnosale = distance*0.21;
+let ticketnosale = distance*0.21;
 const eleSpan3 = document.getElementById("ticketcost")
 eleSpan3.innerHTML = ticketnosale;
 
 
-const elespan4 = document.getElementById ("ticketsales")
-elespan4.innerHTML = ticketwithsale;
+
 
 if (age < 18) {
-  const ticketwithsale = ticketnosale - (ticketnosale * 20 / 100)
+    ticketwithsale = ticketnosale - (ticketnosale * 20 / 100)
 } else {
   
 }
 
 if (age > 65) {
-    const ticketwithsale = ticketnosale - (ticketnosale * 40 / 100)
+    ticketwithsale = ticketnosale - (ticketnosale * 40 / 100)
   } else {
     
   }
+const elespan4 = document.getElementById ("ticketsales")
+elespan4.innerHTML = ticketwithsale;
